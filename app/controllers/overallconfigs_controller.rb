1c1a783978419154ab00cf91e734db2ace626f88
AyumiTakashina <moira.horizon@gmail.com>
2017-09-14 05:11:16 +0000

    view_overallconfig

--- app/controllers/overallconfigs_controller.rb
+++ app/controllers/overallconfigs_controller.rb
@@ -1,5 +1,8 @@
 class OverallconfigsController < ApplicationController
   def index
+    @overall = OverAllConfig.all
+    @artistname = ArtistName.all
+    @musictitle = MusicTitle.all
   end
 
   def show
